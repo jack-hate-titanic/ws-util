41ea38d75bf8d66d9b8829d1878f4671d2c582ad
悦者生存 <1002783067@qq.com>
2022-03-06 22:15:36 +0800

    fix: 修改instanceof

--- src/modules/instanceof.js
+++ src/modules/instanceof.js
@@ -2,24 +2,19 @@
  * @Author: ws
  * @Date: 2021-10-07 09:32:54
  * @LastEditors: ws
- * @LastEditTime: 2021-10-07 09:44:58
+ * @LastEditTime: 2022-03-06 22:13:54
  * @Description:
  */
 
-const instanceOf = (left, right) => {
-  // 获取引用类型的原型
-  let prototype = right.prototype;
-  // 获取值的原型
-  left = left.__proto__;
-  while (true) {
-    if (left === null) {
-      return false;
-    }
-    if (prototype === left) {
+const instanceOf = function (left,right) {
+  let proto = Object.getPrototypeOf(left);
+  while (proto) {
+    if (proto === right.prototype) {
       return true;
     }
-    left = left.__proto__;
+    proto = Object.getPrototypeOf(proto);
   }
-};
+  return false;
+}
 
 module.exports = instanceOf;
